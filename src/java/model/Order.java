8647ad94e4634ab2c3b74808eb9af9ede35c02db
sandeep vani <sandeep.vani@webdunia.net>
2019-05-02 10:52:18 +0530

    Test

---
+++ src/java/model/Order.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Sandeep.vani
+ */
+public class Order {
+    
+    private Integer id;
+    private Integer product_id;
+    private 
+    
+}
